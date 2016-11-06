19b151571ca8ac967a545803222871159a1faad3
Aza Raskin <aza@air.local>
2016-11-05 23:54:46 -0700

    A working generating script

---
+++ examples/iOS/GPUImageJSON/GPUImageJSON/Operations.swift
@@ -0,0 +1,17 @@
+//import Foundation
+//import GPUImage
+//
+//class Operations {
+//    
+//    init(){ }
+//    
+//    func get(name: String) -> BasicOperation? {
+//        switch name {
+//        case "BoxBlur": return BoxBlur()
+//        case "PrewittEdgeDetection": return PrewittEdgeDetection()
+//        case "ColorLocalBinaryPattern": return ColorLocalBinaryPattern()
+//        default: return nil
+//        }
+//    }
+//    
+//}
